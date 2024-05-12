ee357d6065761a3bba5cb57b11d7479374436099
xiaweile <xiaweile@itcast>
2024-05-12 20:57:08 +0800

    更新请求接口内容

--- src/main/java/com/weile/service/ProcessGptService.java
+++ src/main/java/com/weile/service/ProcessGptService.java
@@ -3,7 +3,6 @@ package com.weile.service;
 
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
-import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.weile.request.ContentRequest;
 import com.weile.request.GptParamRequest;
@@ -17,23 +16,20 @@ import java.util.*;
  **/
 public class ProcessGptService {
     @Value(value = "gpt.url")
-    private  static  String gptUrl = "https://stream.api2d.net/v1/chat/completions";
+    private  String gptUrl = "https://oa.api2d.net/v1/chat/completions";
     @Value(value = "gpt.token")
-    private static String gptToken ="Bearer fk190313-KdmEbwOxr9rUXQCkgcggHYKTFNtZqbbV";
-    public  static  HttpResponse processGpt(){
+    private  String gptToken ="Bearer fk190313-KdmEbwOxr9rUXQCkgcggHYKTFNtZqbbV";
+    public   HttpResponse processGpt(){
         ContentRequest contentRequest = new ContentRequest();
         contentRequest.setRole("user");
-        contentRequest.setContent("生成一篇200字文章");
+        contentRequest.setContent("生成一篇卡盟200字文章");
         GptParamRequest gptParamRequest = new GptParamRequest();
         gptParamRequest.setMessages(Collections.singletonList(contentRequest));
         return   HttpUtil.createPost(gptUrl).header("Content-Type", "application/json")
-                .header("Authorization", gptToken)
+                .header("Authorization", gptToken).setSSLProtocol("TLSv1.2")
                 .body(JSONUtil.toJsonStr(gptParamRequest)).execute(true);
+        //todo 调用htmlSeo接口将content传递生成
     }
 
-    public static void main(String[] args) {
-        HttpResponse httpResponse = processGpt();
-        JSONObject jsonObject = JSONUtil.parseObj(httpResponse.body());
-        System.out.println(jsonObject.toString());
-    }
+
 }
