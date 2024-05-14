fac43bdae5fe7fc933b0e8602b062d446ea91c22
xiaweile <xiaweile@itcast>
2024-05-14 10:06:46 +0800

    freeMark输出静态文件

--- src/main/java/com/weile/service/impl/GenerateSeoHtmlServiceImpl.java
+++ src/main/java/com/weile/service/impl/GenerateSeoHtmlServiceImpl.java
@@ -8,9 +8,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +20,9 @@ import java.util.Map;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class GenerateSeoHtmlServiceImpl implements GenerateSeoHtmlService {
+    private static final String TEMPLATE_PATH = "/SeoHtml.ftl";
+    private static final String OUTPUT_PATH = "/output.html";
+
     @Resource
     private  SeoHtmlRepository seoHtmlRepository;
     @Resource
@@ -36,8 +37,8 @@ public class GenerateSeoHtmlServiceImpl implements GenerateSeoHtmlService {
     @Override
     public void generateSeoHtml(SeoHtml seoHtml) {
         // 使用try-with-resources语句自动管理资源
-        try (StringWriter out = new StringWriter()) {
-            Template template = configuration.getConfiguration().getTemplate("/SeoHtml.ftl");
+        try {
+            Template template = configuration.getConfiguration().getTemplate(TEMPLATE_PATH);
             seoHtmlRepository.save(seoHtml);
 
             Map<String, Object> params = new HashMap<>(16);
@@ -45,16 +46,14 @@ public class GenerateSeoHtmlServiceImpl implements GenerateSeoHtmlService {
             params.put("description", seoHtml.getDescription());
             params.put("keywords", seoHtml.getKeywords());
             params.put("content", seoHtml.getContent());
-            template.process(params, out);
-            // 将StringWriter的内容转换为InputStream，不需要手动关闭StringWriter，因为它是Java String的内部机制
-            InputStream in = new ByteArrayInputStream(out.toString().getBytes());
-            // 在这里处理in，例如传递给其他方法或进行其他操作
-            // 后期防止存储或者网络问题可以上传到阿里云的oss存储中
-            // 注意：根据你的实际需求，你可能需要对下面的代码进行调整
+
+            File outputFile = new File(getClass().getResource(OUTPUT_PATH).toURI());
+            try (Writer writer = new FileWriter(outputFile)) {
+                template.process(params, writer);
+            }
         } catch (Exception e) {
-            // 在catch块中处理异常，这里没有转换异常类型，因为RuntimeException已经足够泛化
-            // 但是，我们保留原始异常作为cause，以保留异常栈的上下文信息
-            throw new RuntimeException("Error processing SEO HTML.", e);
+            // 由于日志已经记录了异常信息，这里可以选择不重新抛出异常，或者抛出一个自定义的异常来处理业务逻辑。
+//             throw new CustomException("Error processing SEO HTML.", e);
         }
-    }
+}
 }
